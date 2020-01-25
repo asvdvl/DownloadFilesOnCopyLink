6275a330e34af9bbe6910ae0fd8670ca855628d5
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2020-01-26 01:27:02 +0300

    work state

--- DownloadFilesOnCopyLink/Form1.Designer.cs
+++ DownloadFilesOnCopyLink/Form1.Designer.cs
@@ -28,13 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxStartAndActualIndex = new System.Windows.Forms.TextBox();
+            this.listBoxCopyHistory = new System.Windows.Forms.ListBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBoxPathToFolderSaveFiles = new System.Windows.Forms.TextBox();
+            this.labelDescriptionPathToSaveFiles = new System.Windows.Forms.Label();
+            this.labelDescriptionStartIndex = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBoxLog = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBoxStartAndActualIndex
+            // 
+            this.textBoxStartAndActualIndex.Location = new System.Drawing.Point(291, 20);
+            this.textBoxStartAndActualIndex.Name = "textBoxStartAndActualIndex";
+            this.textBoxStartAndActualIndex.Size = new System.Drawing.Size(45, 20);
+            this.textBoxStartAndActualIndex.TabIndex = 0;
+            this.textBoxStartAndActualIndex.Text = "0";
+            // 
+            // listBoxCopyHistory
+            // 
+            this.listBoxCopyHistory.FormattingEnabled = true;
+            this.listBoxCopyHistory.Location = new System.Drawing.Point(5, 47);
+            this.listBoxCopyHistory.Name = "listBoxCopyHistory";
+            this.listBoxCopyHistory.Size = new System.Drawing.Size(371, 95);
+            this.listBoxCopyHistory.TabIndex = 1;
+            this.listBoxCopyHistory.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBoxCopyHistory_MouseDoubleClick);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(338, 18);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(38, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // textBoxPathToFolderSaveFiles
+            // 
+            this.textBoxPathToFolderSaveFiles.Location = new System.Drawing.Point(5, 19);
+            this.textBoxPathToFolderSaveFiles.Name = "textBoxPathToFolderSaveFiles";
+            this.textBoxPathToFolderSaveFiles.Size = new System.Drawing.Size(178, 20);
+            this.textBoxPathToFolderSaveFiles.TabIndex = 0;
+            this.textBoxPathToFolderSaveFiles.Text = "Path to save files";
+            // 
+            // labelDescriptionPathToSaveFiles
+            // 
+            this.labelDescriptionPathToSaveFiles.AutoSize = true;
+            this.labelDescriptionPathToSaveFiles.Location = new System.Drawing.Point(5, 3);
+            this.labelDescriptionPathToSaveFiles.Name = "labelDescriptionPathToSaveFiles";
+            this.labelDescriptionPathToSaveFiles.Size = new System.Drawing.Size(73, 13);
+            this.labelDescriptionPathToSaveFiles.TabIndex = 3;
+            this.labelDescriptionPathToSaveFiles.Text = "Path to folder ";
+            // 
+            // labelDescriptionStartIndex
+            // 
+            this.labelDescriptionStartIndex.AutoSize = true;
+            this.labelDescriptionStartIndex.Location = new System.Drawing.Point(289, 4);
+            this.labelDescriptionStartIndex.Name = "labelDescriptionStartIndex";
+            this.labelDescriptionStartIndex.Size = new System.Drawing.Size(33, 13);
+            this.labelDescriptionStartIndex.TabIndex = 4;
+            this.labelDescriptionStartIndex.Text = "Index";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(185, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Select path";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(255, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(34, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "OK";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // listBoxLog
+            // 
+            this.listBoxLog.FormattingEnabled = true;
+            this.listBoxLog.Location = new System.Drawing.Point(5, 148);
+            this.listBoxLog.Name = "listBoxLog";
+            this.listBoxLog.Size = new System.Drawing.Size(371, 95);
+            this.listBoxLog.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(381, 433);
+            this.Controls.Add(this.listBoxLog);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelDescriptionStartIndex);
+            this.Controls.Add(this.labelDescriptionPathToSaveFiles);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.listBoxCopyHistory);
+            this.Controls.Add(this.textBoxPathToFolderSaveFiles);
+            this.Controls.Add(this.textBoxStartAndActualIndex);
+            this.Name = "Form1";
+            this.Text = "Download";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxStartAndActualIndex;
+        private System.Windows.Forms.ListBox listBoxCopyHistory;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBoxPathToFolderSaveFiles;
+        private System.Windows.Forms.Label labelDescriptionPathToSaveFiles;
+        private System.Windows.Forms.Label labelDescriptionStartIndex;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBoxLog;
     }
 }
 
--- DownloadFilesOnCopyLink/Form1.cs
+++ DownloadFilesOnCopyLink/Form1.cs
@@ -4,17 +4,114 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
 
 namespace DownloadFilesOnCopyLink
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll")]
+        static extern IntPtr SetClipboardViewer(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool ChangeClipboardChain(IntPtr hWndDel, IntPtr hWndNext);
+
+        [DllImport("user32.dll")]
+        static extern int SendMessage(IntPtr hWnd, int msg, IntPtr wparam1, IntPtr lparam);
+
+        IntPtr hWndNextWnd; //Для хранения указателя на следующее окно
+
+        protected override void WndProc(ref Message m) // переопределяем метод 
+        {
+            switch (m.Msg) //Анализируем сообщение Windows
+            {
+                case (0x0001): // Код WM_CREATE наше окно создано
+                    hWndNextWnd = SetClipboardViewer(this.Handle); // Помещаем наше окно в цепочку и сохраняем указатель на следующее 
+                    break;
+                case (0x0002): // Код WM_DESTROY окно будет разрушено, нужно удалитmся из цепочки буфера обмена
+                    ChangeClipboardChain(this.Handle, hWndNextWnd); // Удаляем наше окно и передаём указатель на следующее окно
+                    break;
+                case (0x030D): // Код WM_CHANGECBCHAIN одно из окон удаено из цепочки, нужно востоновить цепочку
+                    if (m.WParam == hWndNextWnd) // Если удаляемое окно это следующие окно в цепочке
+                        hWndNextWnd = m.LParam; //Следующим окном делаем окно идущее в цепочке за удаляемым 
+                    else if (hWndNextWnd != IntPtr.Zero) // Если дескриптор следующего окна определён
+                        SendMessage(hWndNextWnd, m.Msg, m.WParam, m.LParam);// Посылаем сообщение этому окну
+                    break;
+                case (0x0308): //Код WM_DRAWCLIPBOARD содержимое буфера изменилось можно работать
+                    {
+                        
+                        if (Clipboard.ContainsText())
+                        {
+                            String receivedText = Clipboard.GetText();
+                            listBoxCopyHistory.Items.Add(receivedText);
+                            downloadFile(receivedText);
+                        }
+                        SendMessage(hWndNextWnd, m.Msg, m.WParam, m.LParam);// Посылаем сообщение о изменении бефера дальше по цепочке
+                    }
+                    break;
+            }
+            base.WndProc(ref m); //обращаемся к нашему методу
+        }
+
+        private void downloadFile(String url)
+        {
+            string desktopPath = textBoxPathToFolderSaveFiles.Text;
+            string filename = "";
+            try
+            {
+                Uri uri = new Uri(url);
+                filename = System.IO.Path.GetFileName(uri.LocalPath);
+            }
+            catch(System.UriFormatException exp)
+            {
+                listBoxLog.Items.Add(exp.Message);
+                return;
+            }
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadFileCompleted += wc_DownloadFileCompleted;
+                wc.DownloadFileAsync(new Uri(url), desktopPath + "/" + filename);
+            }
+        }
+
+        private void wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                listBoxLog.Items.Add("The download has been cancelled");
+                return;
+            }
+
+            if (e.Error != null) // We have an error! Retry a few times, then abort.
+            {
+                listBoxLog.Items.Add("An error ocurred while trying to download file");
+
+                return;
+            }
+
+            listBoxLog.Items.Add("File succesfully downloaded");
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxPathToFolderSaveFiles.Text = Application.StartupPath;
+            listBoxCopyHistory.Items.Clear();
+        }
+
+        private void listBoxCopyHistory_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
